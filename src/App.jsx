9dd6d7d22a17013f7c31aad2af63662c5b624b4d
Alexis Salgado <chimalexoficial@gmail.com>
2024-06-29 14:49:47 -0600

    finished project

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,45 @@
+import { useState } from "react";
+import data from './data.js';
+
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState([]);
+
+  const handleCount = (e) => {
+    setCount(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    console.log('handle');
+    e.preventDefault();
+    let amount = parseInt(count);
+    setText(data.slice(0, amount));
+  }
+
+  return <section className="section-center">
+    <h4>Tired of boring lorem ipsum?</h4>
+    <form className="lorem-form" onSubmit={handleSubmit}>
+      <label htmlFor="amount"></label>
+      <input 
+      type="number" 
+      name="amount" 
+      id="amount" 
+      min='1' 
+      step='1' 
+      max='8' 
+      value={count} 
+      onChange={(e) => handleCount(e)} />
+
+      <button 
+      className="btn" 
+      type="submit">Generate</button>
+    </form>
+    <article className="lorem-text">
+      {text.map((textMap, index) => {
+        return <p key={index}>{textMap}</p>
+      })}
+    </article>
+  </section>;
 };
 export default App;
